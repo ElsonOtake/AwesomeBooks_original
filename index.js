d3e74b6375928a9dd56b02e818a9110fa460e379
Elson Otake <elsonotake@yahoo.com.br>
2022-05-29 16:04:42 -0300

    Update new module list books

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import Books from "./modules/books.js";
 import { formStorage, populateStorage, populateNewForm } from "./modules/local_storage.js";
+import { dynamicLoad } from "./modules/list_books.js";
+import { DateTime } from "./modules/luxon.js";
 
 const body = document.querySelector('body');
 const dateTime = document.querySelector('.date_time');
@@ -13,50 +15,6 @@ const contact = document.querySelector('.contact');
 const menuContact = document.querySelector('.menu_contact');
 
 const awesome = new Books();
-
-function dynamicLoad() {
-  (document.querySelector('.list')) && body.removeChild(document.querySelector('.list'));
-
-  const list = document.createElement('div');
-  list.className = 'list';
-  const h1 = document.createElement('h1');
-  h1.innerText = 'All awesome books';
-  list.appendChild(h1);
-
-  for (let i = 0; i < awesome.size(); i += 1) {
-    const div = document.createElement('div');
-    div.className = 'book_line';
-    const p = document.createElement('p');
-    p.className = 'book_detail';
-    p.innerText = `"${awesome.nthTitle(i)}" by ${awesome.nthAuthor(i)}`;
-    div.appendChild(p);
-    const button = document.createElement('button');
-    button.className = 'remove_btn';
-    button.type = 'button';
-    button.innerText = 'Remove';
-    div.appendChild(button);
-    list.appendChild(div);
-  }
-  dateTime.insertAdjacentElement('afterend', list);
-  const removeButton = document.querySelectorAll('.remove_btn');
-
-  removeButton.forEach((btn) => btn.addEventListener('click', (e) => {
-    const titleByAuthor = e.target.previousSibling.innerText;
-    const title = titleByAuthor.slice(1, titleByAuthor.lastIndexOf('" by '));
-    const author = titleByAuthor.slice(titleByAuthor.lastIndexOf('" by ') + 5);
-    awesome.remove(title, author);
-    dynamicLoad();
-  }));
-
-  menuList.addEventListener('click', () => {
-    dynamicLoad();
-    menuList.classList.add('active');
-    addNew.classList.add('inactive');
-    menuAddNew.classList.remove('active');
-    contact.classList.add('inactive');
-    menuContact.classList.remove('active');
-  });
-}
 
 newBookBtn.addEventListener('click', () => {
   awesome.add(inputTitle.value, inputAuthor.value);
@@ -97,7 +55,6 @@ if (!localStorage.getItem('data')) {
 
 (localStorage.getItem('classData')) && awesome.restoreStorage();
 
-dynamicLoad();
+dynamicLoad(awesome);
 
-import { DateTime } from "./modules/luxon.js";
 setInterval(() => dateTime.innerText = DateTime.now().toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS), 1000);
